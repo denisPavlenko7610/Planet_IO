0bf310b0aafc5ba5154cc3b028be7c3d24e2f0f7
Denis Pavlenko <denispavlenko7610@gmail.com>
2022-07-23 12:18:52 +0300

    add start scene

---
+++ Assets/Scripts/Core/LoadGame.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+namespace PlanetIO_Core
+{
+    public class LoadGame : MonoBehaviour
+    {
+        [SerializeField] private UIDocument _uiDocument;
+        
+        private Button _startButton;
+        string playbutton = "PlayButton";
+
+        void Start()
+        {
+            var root = _uiDocument.rootVisualElement;
+            _startButton = root.Q<Button>(playbutton);
+            _startButton.clicked += StartButtonPressed;
+        }
+
+        void StartButtonPressed()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        private void OnDisable()
+        {
+            _startButton.clicked -= StartButtonPressed;
+        }
+    }
+}
