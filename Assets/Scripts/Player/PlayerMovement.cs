a1d14d98d51e0049e8f9cafc9705bca9f622af25
Denis Pavlenko <denispavlenko7610@gmail.com>
2022-08-09 14:32:00 +0300

    add background

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using Dythervin.AutoAttach;
+using UnityEngine;
+
+namespace PlanetIO
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class PlayerMovement : MonoBehaviour
+    {
+        [SerializeField, Attach(Attach.Scene)] private Camera mainCamera;
+        [SerializeField, Attach] private Rigidbody2D rigidbody2D;
+        [SerializeField] private float speed = 3f;
+        private Vector2 mousePosition;
+
+        void Update()
+        {
+            mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            transform.Translate(mousePosition.normalized * (speed * Time.deltaTime));
+        }
+
+        private void FixedUpdate()
+        {
+        }
+    }
+}
