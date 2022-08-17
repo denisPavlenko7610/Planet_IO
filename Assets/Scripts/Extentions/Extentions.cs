3fc056d61164ee36b0265c7913b41dbc6d8eb51f
Denis Pavlenko <denispavlenko7610@gmail.com>
2022-08-17 09:52:46 +0300

    added extentions

---
+++ Assets/Scripts/Extentions/Extentions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Extentions
+{
+    public static class Extentions
+    {
+        public static T With<T>(this T self, Action<T> set)
+        {
+            set.Invoke(self);
+            return self;
+        }
+
+        public static T With<T>(this T self, Action<T> apply, Func<bool> when)
+        {
+            if (when())
+                apply?.Invoke(self);
+
+            return self;
+        }
+
+        public static T With<T>(this T self, Action<T> apply, bool when)
+        {
+            if (when)
+                apply?.Invoke(self);
+
+            return self;
+        }
+    }
+}
