c52e6c70848137f0cdd23eb3d7c5fcaa360ac1c8
Denis Pavlenko <denispavlenko7610new@gmail.com>
2023-01-03 23:55:03 +0200

    add netcode and multiplayer

---
+++ Assets/Scripts/Core/Network/NetworkUI.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using TMPro;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Planet_IO.Core.Network
+{
+    public class NetworkUI : NetworkBehaviour
+    {
+        [SerializeField] private Button _hostButton;
+        [SerializeField] private Button _clientButton;
+        [SerializeField] private TextMeshProUGUI _playerText;
+
+        private NetworkVariable<int> playerNum = new(0,NetworkVariableReadPermission.Everyone);
+
+        private void Awake()
+        {
+            Subscribe();
+        }
+
+        private void Subscribe()
+        {
+            _hostButton.onClick.AddListener(() => { NetworkManager.Singleton.StartHost(); });
+            _clientButton.onClick.AddListener(() => { NetworkManager.Singleton.StartClient(); });
+        }
+
+        private void Update()
+        {
+            UpdatePlayersCount();
+        }
+
+        private void UpdatePlayersCount()
+        {
+            _playerText.text = "Players: " + playerNum.Value;
+
+            if (!IsServer)
+                return;
+
+            playerNum.Value = NetworkManager.Singleton.ConnectedClients.Count;
+        }
+    }
+}
