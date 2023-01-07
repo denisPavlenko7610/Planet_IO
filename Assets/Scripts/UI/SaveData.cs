e21a33b4e54d0e80ee35897689a95e288de832e8
Roman Kopylov <ze6est1@yandex.ru>
2023-01-07 18:43:47 +0300

    SaveDada finished

--- Assets/Scripts/UI/SaveData.cs
+++ Assets/Scripts/UI/SaveData.cs
@@ -5,6 +5,18 @@ namespace Planet_IO
 {
     public class SaveData : MonoBehaviour
     {
-        [SerializeField] private TextMeshProUGUI _nickname;
+        [SerializeField] public TMP_InputField _inputField;
+
+        private string _nickname;
+
+        void Start()
+        {
+            _inputField.onEndEdit.AddListener(delegate { SetInput(_inputField); });
+        }
+
+        private void SetInput(TMP_InputField userInput)
+        {
+            string _nickname = userInput.text;            
+        }
     }
 }
