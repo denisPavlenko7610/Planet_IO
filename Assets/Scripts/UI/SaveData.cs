692921ed19fe93ef78090c8fb0fc78e634e70c06
Roman Kopylov <ze6est1@yandex.ru>
2023-01-07 23:07:46 +0300

    Random Nickname

--- Assets/Scripts/UI/SaveData.cs
+++ Assets/Scripts/UI/SaveData.cs
@@ -1,23 +1,44 @@
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Planet_IO
 {
     public class SaveData : MonoBehaviour
     {
         [SerializeField] private TMP_InputField _inputField;
+        [SerializeField] private Button _setRandomNickname;
 
+        private List<string> _inputNames = new List<string>() { "Bob", "Tom", "Riki", "Rock", "Margaret", "Monika" };
         private string _nickname;
 
-        private void Start()
+        private void OnEnable()
         {
-            _inputField.onEndEdit.AddListener(_nickname => SetInput(_inputField) );
+            _setRandomNickname.onClick.AddListener(SetNickname);
+        }
+
+        private void OnDisable()
+        {
+            _setRandomNickname.onClick.RemoveListener(SetNickname);
+        }
+
+        private void Start()
+        {            
+            _inputField.onEndEdit.AddListener(_nickname => SetInput(_inputField));
         }
 
         private void SetInput(TMP_InputField userInput)
         {
-            _nickname = userInput.text;
-            Debug.Log(_nickname);
+            _nickname = userInput.text;            
+        }
+
+        private void SetNickname() 
+        {
+            int arrayCount = _inputNames.Count;
+            int numberElement = Random.Range(0, arrayCount);
+
+            _inputField.text = _inputNames[numberElement];            
         }
     }
 }
